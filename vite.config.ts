654a17a83c8c9f5921410403311979f25c78c19a
Eason Chiu <76670758@qq.com>
2025-12-25 15:41:08 +0800

    chore: 支持测试环境在 .env 文件中修改 base api url (#6)
    Co-authored-by: Eason Chiu <easonchiu19860129@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -6,12 +6,18 @@ import getAppConfig from './app.config';
 export default defineConfig(({ mode, command }) => {
   // 1. 加载环境变量（从 .env 文件）
   const env = loadEnv(mode, process.cwd(), '');
+  const isDev = env.VITE_USER_NODE_ENV === 'development';
   
   // 2. 获取 .env 中的版本配置
   const appEdition = env.VITE_APP_EDITION || 'cn';
 
   // 3. 根据版本获取应用配置
   const appConfig = getAppConfig(env.UNI_UTS_PLATFORM as any, appEdition as any);
+
+  // 4. 如果开发环境且环境变量中配置了 BASE_API_URL，则使用环境变量中的值，优先级高于 app.config.ts 中的值
+  if (isDev && env.VITE_BASE_API_URL) {
+    appConfig.BASE_API_URL = env.VITE_BASE_API_URL;
+  }
 
   return {
     css: {
