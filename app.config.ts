c5cd81dbde87df17c2ac591d59f3a3b51135ac9e
Eason Chiu <easonchiu19860129@gmail.com>
2025-12-26 18:55:28 +0800

    chore: 更新 app 配置

--- app.config.ts
+++ app.config.ts
@@ -14,54 +14,54 @@ type Platform = 'mp-weixin' | 'app-ios' | 'app-android' | 'app-harmony';
  */
 export default function getAppConfig(platform: Platform, appEdition: 'full' | 'cn' | 'intl'): Record<string, any> {
   // 基础 API 地址
-  const BASE_API_URL = appEdition === 'intl' ? 'http://204.141.229.218:8001' : 'http://111.62.241.103:8001';
+  const BASE_API_URL = appEdition === 'intl' ? 'http://47.79.126.80:8001' : 'http://47.112.207.28:8001';
 
   // 是否支持微信小程序手机号登录
   const SUPPORT_LOGIN_TYPE_WX_MP_PHONE = appEdition === 'cn' || appEdition === 'full';
 
   // 是否支持游客登录
-  const SUPPORT_LOGIN_TYPE_GUEST = appEdition === 'full';
+  const SUPPORT_LOGIN_TYPE_GUEST = false;
 
   // 在小程序中是否支持游客登录
-  const SUPPORT_LOGIN_TYPE_GUEST_MP = appEdition === 'cn' || appEdition === 'full';
+  const SUPPORT_LOGIN_TYPE_GUEST_MP = false;
 
   // 是否支持谷歌登录
   const SUPPORT_LOGIN_TYPE_GOOGLE = platform === 'app-android' && appEdition !== 'cn';
 
   // 是否支持密码登录
-  const SUPPORT_LOGIN_TYPE_PASSWORD = true;
+  const SUPPORT_LOGIN_TYPE_PASSWORD = false;
 
   // 是否支持微信授权登录
-  const SUPPORT_LOGIN_TYPE_WECHAT_OAUTH = true;
+  const SUPPORT_LOGIN_TYPE_WECHAT_OAUTH = appEdition == 'cn';
 
   // 是否支持邮箱登录
-  const SUPPORT_LOGIN_TYPE_EMAIL = appEdition === 'intl' || appEdition === 'full';
+  const SUPPORT_LOGIN_TYPE_EMAIL = true;
 
   // 是否支持苹果登录
   const SUPPORT_LOGIN_TYPE_APPLE = platform === 'app-ios';
 
   // 是否支持短信登录
-  const SUPPORT_LOGIN_TYPE_SMS = appEdition === 'cn' || appEdition === 'full';
+  const SUPPORT_LOGIN_TYPE_SMS = appEdition === 'cn';
 
   // 蓝牙配网是否启用设备名称筛选，可选值：true, false，默认 true
   const BLE_FILTER_ENABLED = true;
 
   // 控制首页设置引导的显示
   // 可选值: qrcode, bluetooth, both (不填默认为 both)
-  const APP_SETUP_MODE = 'both';
+  const APP_SETUP_MODE = 'qrcode';
 
   // 是否支持声纹
-  const APP_USE_VOICEPRINT = true;
+  const APP_USE_VOICEPRINT = false;
 
   // 用户协议和隐私政策 URL
-  const TERMS_URL = 'https://www.qiniu.com/agreements/user-agreement';
-  const PRIVACY_URL = 'https://www.qiniu.com/agreements/privacy-right';
+  const TERMS_URL = 'https://mengdiantansuo.com.cn/terms';
+  const PRIVACY_URL = 'https://mengdiantansuo.com.cn/privacy';
 
   // Google 授权登录，海外版需要配置 Google Client ID
   const GOOGLE_OAUTH_CLIENT_ID_WEB =
     appEdition === 'cn'
       ? ''
-      : '668197306109-2glguqobqtbm92rt9upnot9rrt0t7bmf.apps.googleusercontent.com';
+      : '287792291942-4i16oc73btg889o3l8hf174980d4f7po.apps.googleusercontent.com';
 
   // ARMS 监控配置
   const ARMS_PID = 'hmn73dtabu@f6272e69e2e3597';
